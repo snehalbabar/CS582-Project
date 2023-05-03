46635aede1ed8bddfaf6c2ab9dceed8129b975e3
snehalbabar <h728e344@wichita.edu>
2023-05-02 23:26:36 -0500

    16. createed Dyanamic URLS

--- src/products/views.py
+++ src/products/views.py
@@ -27,28 +27,20 @@ from .models import product
 #     context = { }
 #     return render(request,"product/create.html", context)
 
-def render_inital_data(request):
-    inital_data = {
-        'title':"this is my awsome title",
-    }
-    obj = product.objects.get(id=1)
-    form = RawProductForm(request.POST or None, initial=inital_data, instance=obj)
-    if form.is_valid():
-        form.save() 
-    context = {
-        'form' : form
-    }
-    return render(request,"product/create.html", context)
-
-# def product_create_view(request):
-#     form = ProductForm(request.POST or None)
+# def render_inital_data(request):
+#     inital_data = {
+#         'title':"this is my awsome title",
+#     }
+#     obj = product.objects.get(id=1)
+#     form = RawProductForm(request.POST or None, initial=inital_data, instance=obj)
 #     if form.is_valid():
-#         form.save()
-#         form = ProductForm()
+#         form.save() 
 #     context = {
 #         'form' : form
 #     }
 #     return render(request,"product/create.html", context)
+
+ 
 
 
 def product_detail_view(request):
@@ -62,3 +54,16 @@ def product_detail_view(request):
         'object' : obj
     }
     return render(request,"product/detail.html", context)
+
+# def dymanic_lookup_view(request,my_id):
+#     obj = product.objects.get(my_id)
+#     # context = {
+#     #     "title" :obj.title,
+#     #     "desc" : obj.decs,
+#     #     "price" :  obj.price        
+#     # }
+#     context = {
+#         'object' : obj
+#     }
+#     return render(request,"product/detail.html", context)
+
--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view,contact_view
-from products.views import product_detail_view, product_create_view
+from products.views import product_detail_view, product_create_view,dymanic_lookup_view
 
 
 urlpatterns = [
@@ -26,4 +26,5 @@ urlpatterns = [
     path("contact/", contact_view, name='contact'),
     path("product/", product_detail_view),
     path("create/", product_create_view),
+    path("lookup/<int:my_id>", dymanic_lookup_view),
 ]
