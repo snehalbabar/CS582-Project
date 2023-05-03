3f42c59b5ec6302a8d29a85adb78dcdf633542e6
snehalbabar <h728e344@wichita.edu>
2023-05-03 12:18:27 -0500

    30. added models and view to course module

--- src/courses/views.py
+++ src/courses/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
 
-# Create your views here.
+from .models import Course 
+# BASE VIEW CLass = VIEW
+
+class CourseObjectMixin(object):
+    model = Course
+    def get_object(self):
+        id = self.kwargs.get('id')
+        obj = None
+        if id is not None:
+            obj = get_object_or_404(self.model, id=id)
+        return obj 
