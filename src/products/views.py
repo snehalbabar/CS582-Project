d1aecf9247a423dc1323394966d2ebd1619d9e09
snehalbabar <h728e344@wichita.edu>
2023-05-02 23:30:20 -0500

    17. Resolve error in dymanic urls

--- src/products/views.py
+++ src/products/views.py
@@ -1,22 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .forms import ProductForm, RawProductForm
 from .models import product
 
 # Create your views here.
 
-# def product_create_view(request):
-#     my_form = RawProductForm()
-#     if request.method == "POST":
-#         my_form = RawProductForm(request.POST)
-#         if my_form.is_valid():
-#             print(my_form.cleaned_data)
-#             product.objects.create(**my_form.cleaned_data)
-#         else:
-#             print(my_form.errors)
-#     context = { 
-#         'form': my_form
-#     }
-#     return render(request,"product/create.html", context)
+def product_create_view(request):
+    my_form = RawProductForm()
+    if request.method == "POST":
+        my_form = RawProductForm(request.POST)
+        if my_form.is_valid():
+            print(my_form.cleaned_data)
+            product.objects.create(**my_form.cleaned_data)
+        else:
+            print(my_form.errors)
+    context = { 
+        'form': my_form
+    }
+    return render(request,"product/create.html", context)
 
 
 #RAW HTML Method
@@ -55,15 +55,15 @@ def product_detail_view(request):
     }
     return render(request,"product/detail.html", context)
 
-# def dymanic_lookup_view(request,my_id):
-#     obj = product.objects.get(my_id)
-#     # context = {
-#     #     "title" :obj.title,
-#     #     "desc" : obj.decs,
-#     #     "price" :  obj.price        
-#     # }
-#     context = {
-#         'object' : obj
-#     }
-#     return render(request,"product/detail.html", context)
+def dymanic_lookup_view(request,my_id):
+    obj = product.objects.get(id=my_id)
+    # context = {
+    #     "title" :obj.title,
+    #     "desc" : obj.decs,
+    #     "price" :  obj.price        
+    # }
+    context = {
+        'object' : obj
+    }
+    return render(request,"product/detail.html", context)
 
