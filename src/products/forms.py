8e6d9a6d5635e6982352c1a8be286059edfcd0e3
snehalbabar <h728e344@wichita.edu>
2023-05-02 22:33:16 -0500

    13. Apply form widgets

--- src/products/forms.py
+++ src/products/forms.py
@@ -13,6 +13,17 @@ class ProductForm(forms.ModelForm):
 
 
 class RawProductForm(forms.Form):
-    title = forms.CharField()
-    decs  = forms.CharField()
-    price = forms.DecimalField()
+    title = forms.CharField(label=" ", widget=forms.Textarea(attrs={
+                                "placeholder":"enter title",
+                            }
+                                                  ))
+    decs  = forms.CharField(required=False,
+                            widget=forms.Textarea(attrs={
+                                "placeholder":"enter decsription",
+                                "class":"new-class",
+                                "id":"my_id",
+                                "rows":20,
+                                "cols":100,
+                            }
+                                                  ))
+    price = forms.DecimalField(initial=0.99)
