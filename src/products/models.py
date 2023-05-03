7e23a128fd904b3cdef7c011759c0f2cd88038e6
snehalbabar <h728e344@wichita.edu>
2023-05-02 19:40:01 -0500

    1.creating basic structure for project

--- src/products/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class product(models.Model):
-    title   = models.CharField(max_length=120)
-    decs   = models.TextField(blank=True, null=True)
-    price  = models.DecimalField(decimal_places=2,max_digits=200)
-    summry = models.TextField()
-    featured = models.BooleanField( )
